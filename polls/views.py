ba8cccd82452e52b32b35ab3b1b619fa0faf5428
RFNshare <rfnshare@gmail.com>
2021-06-06 21:40:44 +0600

    Test

--- polls/views.py
+++ polls/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. bf65f07b is the polls index.")
+    return HttpResponse("Hello, world. is the polls index.")
