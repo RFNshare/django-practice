9171264a4a5fcf0400ea03f464d7489fd4db54a6
RFNshare <rfnshare@gmail.com>
2021-07-10 15:29:07 +0600

    Session count

--- hello/views.py
+++ hello/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def myview(request):
-    resp = HttpResponse('C is for cookie and that is good enough for me...')
+    num_visits = request.session.get('num_visits', 0) + 1
+    request.session['num_visits'] = num_visits
+    if num_visits > 4:
+        del(request.session['num_visits'])
+    resp = HttpResponse('view count=' + str(num_visits))
     resp.set_cookie('dj4e_cookie', '579036d9', max_age=1000)
     return resp
