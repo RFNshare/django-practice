e1cfeccc3b50105752b8d78daf1006274083990c
RFNshare <rfnshare@gmail.com>
2021-08-08 21:44:11 +0600

    Advance Setup for assigment

--- mysite/settings.py
+++ mysite/settings.py
@@ -91,10 +91,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': str(BASE_DIR / 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'drchuck$ads',
+        'USER': 'drchuck',
+        'PASSWORD': 'phone_8675309',
+        'HOST': 'drchuck.mysql.pythonanywhere-services.com',
+         'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
